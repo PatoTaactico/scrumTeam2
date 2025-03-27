9372d3853dde28dafb7001f91807948398d1c46b
Cristian-Gomez <ycamigo123@gmail.com>
2025-03-27 12:44:45 -0500

    se acaba de crear la tabla de Ordenes_compraGUI.form

--- src/Ordenes_compra/Ordenes_compraGUI.java
+++ src/Ordenes_compra/Ordenes_compraGUI.java
@@ -4,4 +4,12 @@ import javax.swing.*;
 
 public class Ordenes_compraGUI {
     private JPanel main;
+    private JTable table1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JComboBox comboBox1;
+    private JTextField textField5;
+    private JTextField textField6;
 }
