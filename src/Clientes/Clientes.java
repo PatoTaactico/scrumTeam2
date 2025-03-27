ef22be08955dc489995dafe5022be01b07b0449d
Karen Valencia <karenvalencia717@gmail.com>
2025-03-27 14:22:01 -0500

    implementacion de clientes.java

---
+++ src/Clientes/Clientes.java
@@ -0,0 +1,36 @@
+package Clientes;
+
+public class Clientes{
+     int id_cliente;
+     String nombre;
+     String telefono;
+     String direccion;
+     String correo;
+
+    public Clientes(int id_cliente, String nombre, String telefono, String direccion, String correo)
+    {
+        this.id_cliente =id_cliente;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.correo = correo;
+    }
+
+    public int getId() { return id_cliente; }
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+
+    public String getDireccion() { return direccion; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
+}
+
