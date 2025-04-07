310c02dc9c0d3ddd2971c8dca7b2c1aff74ebf3f
Cristian-Gomez <ycamigo123@gmail.com>
2025-04-06 19:49:41 -0500

    se agrego la base de datos al archivo

---
+++ bd/ferreteria.sql
@@ -0,0 +1,252 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-04-2025 a las 07:18:53
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ferreteria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id_cliente` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `telefono` varchar(50) DEFAULT NULL,
+  `direccion` varchar(50) DEFAULT NULL,
+  `correo` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id_cliente`, `nombre`, `telefono`, `direccion`, `correo`) VALUES
+(3, 'cliente 3', '567567', 'carrera 6', '3333'),
+(4, 'diana', '312', 'calle31', 'ygmail');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+CREATE TABLE `empleados` (
+  `id_empleado` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `cargo` enum('administrador','vendedor') NOT NULL,
+  `salario` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `empleados`
+--
+
+INSERT INTO `empleados` (`id_empleado`, `nombre`, `cargo`, `salario`) VALUES
+(1, 'pedro', 'administrador', 1200000),
+(2, 'juan2', 'administrador', 3000000);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inventario_productos`
+--
+
+CREATE TABLE `inventario_productos` (
+  `id_producto` int(11) NOT NULL,
+  `nombre_producto` varchar(50) NOT NULL,
+  `categoria` varchar(50) DEFAULT NULL,
+  `cantidad_stock` int(11) DEFAULT 0,
+  `precio_producto` int(10) NOT NULL,
+  `id_proveedor_asociado` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ordenes_compra`
+--
+
+CREATE TABLE `ordenes_compra` (
+  `id_orden_compra` int(11) NOT NULL,
+  `id_cliente` int(11) DEFAULT NULL,
+  `id_empleado` int(11) DEFAULT NULL,
+  `total` int(10) DEFAULT NULL,
+  `estado_orden` enum('pendiente','pagada','enviada') DEFAULT 'pendiente',
+  `fecha_compra` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedores`
+--
+
+CREATE TABLE `proveedores` (
+  `id_proveedor` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `telefono` varchar(50) DEFAULT NULL,
+  `categoria_producto` enum('herramientas','electricidad','materiales','construccion') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `proveedores`
+--
+
+INSERT INTO `proveedores` (`id_proveedor`, `nombre`, `telefono`, `categoria_producto`) VALUES
+(4, 'FerreMax', '555-1234', 'herramientas'),
+(5, 'ElectroPlus', '555-5678', 'electricidad'),
+(6, 'ConstruMarket', '555-7891', 'construccion'),
+(7, 'marcelo', '555-3210', 'materiales'),
+(8, 'TodoHerramientas', '555-9012', 'herramientas'),
+(9, 'SuministrosPro', '555-3456', 'materiales'),
+(11, 'MegaTools', '555-1122', 'herramientas'),
+(12, 'PowerHouse', '555-3344', 'electricidad'),
+(13, 'ConstruMaster', '555-5566', 'construccion'),
+(14, 'jhoan', '555-6669', 'construccion');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro_ventas`
+--
+
+CREATE TABLE `registro_ventas` (
+  `id_venta` int(11) NOT NULL,
+  `id_orden_compra` int(11) DEFAULT NULL,
+  `id_producto` int(11) DEFAULT NULL,
+  `cantidad` int(11) NOT NULL,
+  `sub_total` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indices de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD PRIMARY KEY (`id_empleado`);
+
+--
+-- Indices de la tabla `inventario_productos`
+--
+ALTER TABLE `inventario_productos`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `id_proveedor_asociado` (`id_proveedor_asociado`);
+
+--
+-- Indices de la tabla `ordenes_compra`
+--
+ALTER TABLE `ordenes_compra`
+  ADD PRIMARY KEY (`id_orden_compra`),
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `id_empleado` (`id_empleado`);
+
+--
+-- Indices de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD PRIMARY KEY (`id_proveedor`);
+
+--
+-- Indices de la tabla `registro_ventas`
+--
+ALTER TABLE `registro_ventas`
+  ADD PRIMARY KEY (`id_venta`),
+  ADD KEY `id_orden_compra` (`id_orden_compra`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `inventario_productos`
+--
+ALTER TABLE `inventario_productos`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `ordenes_compra`
+--
+ALTER TABLE `ordenes_compra`
+  MODIFY `id_orden_compra` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  MODIFY `id_proveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `registro_ventas`
+--
+ALTER TABLE `registro_ventas`
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `inventario_productos`
+--
+ALTER TABLE `inventario_productos`
+  ADD CONSTRAINT `inventario_productos_ibfk_1` FOREIGN KEY (`id_proveedor_asociado`) REFERENCES `proveedores` (`id_proveedor`);
+
+--
+-- Filtros para la tabla `ordenes_compra`
+--
+ALTER TABLE `ordenes_compra`
+  ADD CONSTRAINT `ordenes_compra_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`),
+  ADD CONSTRAINT `ordenes_compra_ibfk_2` FOREIGN KEY (`id_empleado`) REFERENCES `empleados` (`id_empleado`);
+
+--
+-- Filtros para la tabla `registro_ventas`
+--
+ALTER TABLE `registro_ventas`
+  ADD CONSTRAINT `registro_ventas_ibfk_1` FOREIGN KEY (`id_orden_compra`) REFERENCES `ordenes_compra` (`id_orden_compra`),
+  ADD CONSTRAINT `registro_ventas_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `inventario_productos` (`id_producto`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
